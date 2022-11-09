fa48dabbb39976d226a46b01d37a2d691a55e8d7
iarmolla <ivan.armolla@gmail.com>
2022-11-09 19:10:00 -0300

    saving state

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,11 +1,14 @@
 import actionTypes from '../actionTypes'
 
 const initialState = {}
+const saveUser = (state,action) => {
+  return {...state,values:action.values}
+}
 
 export default function usersReducer(state = initialState, action) {
   switch (action.type) {
-    case actionTypes.GET_USER:
-      return state, action
+    case actionTypes.GET_USER_SUCCESS:
+      return saveUser(state, action)
     default:
       return state
   }
