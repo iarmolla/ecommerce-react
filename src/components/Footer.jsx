d0ea2154013fafa733bb30988f1073f47f7d1409
iarmolla <ivan.armolla@gmail.com>
2022-11-16 02:20:59 -0300

    changing footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import '../styles/footer.css'
 
 function Footer() {
   return (
     <div>
-      <footer className="py-3 my-4">
+      <footer className="footer py-3 my-4">
         <p className="border-bottom"></p>
         <p className="text-center text-muted">© 2022 Company, Inc</p>
       </footer>
