8be88abe21558e6d8551ee37f922addb89418004
iarmolla <ivan.armolla@gmail.com>
2023-01-04 07:08:10 -0300

    adding users

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,9 +1,12 @@
 import { all, fork } from 'redux-saga/effects';
 import products from './productsSagas';
+import users from './usersSagas';
+
 
 function* rootSaga () {
   yield all([
     fork(products),
+    fork(users)
   ]);
 }
 
