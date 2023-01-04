ca6b1affeffa104334f8adf9a8265f740a28c7a6
iarmolla <ivan.armolla@gmail.com>
2023-01-04 07:07:26 -0300

    adding local storage

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/navbar.css";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
-function NavBar() {
+function NavBar({ ...props }) {
+  const [user, setUser] = useState(false)
+  const [userName, setUserName] = useState()
+  useEffect(() => {
+    let aux = window.localStorage.getItem("loggedUser");
+    if (aux) {
+      aux = JSON.parse(aux)
+      setUser(true)
+      setUserName(aux.email)
+    }
+    else {
+      Object.values(props.getUsers).map((user) => {
+        if (user.logged) {
+          setUser(true)
+          setUserName(user.email)
+        }
+      })
+    }
+  }, [props.getUsers])
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-light">
@@ -54,34 +72,65 @@ function NavBar() {
                   Joyeria
                 </Link>
               </motion.div>
-              <div className="navbar-icons--">
-                <motion.div
-                  whileTap={{ scale: 0.9 }}>
-                  <Link to="/login">
-                    <box-icon name="user"></box-icon>
-                  </Link>
-                </motion.div>
-                <motion.div
-                  whileTap={{ scale: 0.9 }}>
-                  <Link to="/cart">
-                    <box-icon name="cart"></box-icon>
-                  </Link>
-                </motion.div>
-              </div>
+              {
+                user ? <div className="navbar-icons--">
+                  <span>{userName}</span>
+                  <motion.div className="navbar-icons--"
+                    whileTap={{ scale: 0.9 }}>
+                    <Link to="/cart" className="navbar-icons--">
+                      <box-icon name="cart" className="navbar-icons--"></box-icon>
+                    </Link>
+                  </motion.div>
+                </div>
+                  : <>
+                    <div className="navbar-icons--">
+                      <motion.div
+                        whileTap={{ scale: 0.9 }}>
+                        <Link to="/login">
+                          <box-icon name="user"></box-icon>
+                        </Link>
+                      </motion.div>
+                      <motion.div
+                        whileTap={{ scale: 0.9 }}>
+                        <Link to="/cart">
+                          <box-icon name="cart"></box-icon>
+                        </Link>
+                      </motion.div>
+                    </div>
+                  </>
+              }
             </div>
             <div className="navbar-icons">
-              <motion.div
-                whileTap={{ scale: 0.9 }}>
-                <Link to="/login">
-                  <box-icon name="user"></box-icon>
-                </Link>
-              </motion.div>
-              <motion.div
-                whileTap={{ scale: 0.9 }}>
-                <Link to="/cart">
-                  <box-icon name="cart"></box-icon>                  
-                </Link>
-              </motion.div>
+              {
+                user ? <div className="navbar-icons">
+                  <span>{userName}</span>
+                  <motion.div className="navbar-icons"
+                    whileTap={{ scale: 0.9 }}>
+                    <Link to="/cart" className="navbar-icons">
+
+                      <box-icon name="cart" className="navbar-icons"></box-icon>
+                    </Link>
+                  </motion.div>
+                </div>
+                  : <>
+                    <motion.div
+                      whileTap={{ scale: 0.9 }}>
+                      <Link to="/login">
+                        <div className="navbar-icons">
+                          <box-icon name="user"></box-icon>
+                        </div>
+                      </Link>
+                    </motion.div>
+                    <motion.div
+                      whileTap={{ scale: 0.9 }}>
+                      <Link to="/cart" >
+                        <div className="navbar-icons">
+                          <box-icon name="cart"></box-icon>
+                        </div>
+                      </Link>
+                    </motion.div>
+                  </>
+              }
             </div>
           </div>
         </div>
