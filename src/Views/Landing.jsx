17fa02e115f43861e75a45ea55b7bff46265a58f
iarmolla <ivan.armolla@gmail.com>
2023-01-05 03:32:40 -0300

    adding scroll into

--- src/Views/Landing.jsx
+++ src/Views/Landing.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../styles/landing.css'
 import { Link } from "react-router-dom";
 import Footer from '../components/Footer'
 import samsung from '../assets/samsung.jpg'
 
 function Landing() {
+    useEffect(() => {
+        const element = document.getElementById('landing-title')
+        element?.scrollIntoView()
+    }, [])
     return (
         <>
             <div className='landing'>
                 <section className='landing-section'>
-                    <h1 className='landing-title'>Mejores productos al alcance de tus manos</h1>
+                    <h1 className='landing-title' id="landing-title">Mejores productos al alcance de tus manos</h1>
                     <p className='landing-text'>Ultimos modelos actualizados con los precios mas bajos</p>
                     <div className='landing-buttons'>
                         <Link to='/' className='card-button landing-button'>Mas productos</Link>
