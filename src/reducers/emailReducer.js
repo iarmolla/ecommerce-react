19b2c97a148652f0678dcbf6aa64f4738cb67263
iarmolla <ivan.armolla@gmail.com>
2023-01-09 05:08:38 -0300

    adding emailReducer

--- src/reducers/emailReducer.js
+++ src/reducers/emailReducer.js
@@ -1,11 +1,12 @@
 const initialState = [];
-
 export default function products(state = initialState, action) {
   switch (action.type) {   
-    case "VALIDATE_EMAIL":
-      return state.filter((element) => {
-        return element.id != action.id;
-      });
+    case "VALIDATE_EMAIL_SUCCESS":
+
+      return {
+        ...state,
+        ...action.email
+      }
     default:
       return state;
   }
