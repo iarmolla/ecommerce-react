3e83cf549018ec68b00aeabf16151350090200a6
iarmolla <ivan.armolla@gmail.com>
2023-01-09 05:08:23 -0300

    adding logout

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,7 +12,7 @@ function NavBar({ ...props }) {
     if (aux) {
       aux = JSON.parse(aux)
       setUser(true)
-      setUserName(aux.email) 
+      setUserName(aux.email)
     }
     else {
       Object.values(props.getUsers).map((user) => {
@@ -23,6 +23,12 @@ function NavBar({ ...props }) {
       })
     }
   }, [props.getUsers])
+  const logout = () => {
+    setUser(false)
+    setUserName()
+    let aux = window.localStorage.getItem("loggedUser");
+    aux = window.localStorage.clear()
+  }
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-light">
@@ -73,42 +79,68 @@ function NavBar({ ...props }) {
                   Joyeria
                 </Link>
               </motion.div>
-              {
-                user ? <div className="navbar-icons--">
-                  <span>{userName}</span>
-                  <motion.div className="navbar-icons--"
+              {user ?
+                <div className="navbar-icons--">
+                  <div className="dropdown">
+                    <button
+                      className="btn btn-light men-order-button dropdown-toggle"
+                      type="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      {userName}
+                    </button>
+                    <ul className="dropdown-menu">
+                      <li>
+                        <span className="dropdown-item" onClick={() => logout()}>Cerrar sesion</span>
+                      </li>
+                    </ul>
+                  </div>
+                  <motion.div
                     whileTap={{ scale: 0.9 }}>
-                    <Link to="/cart" className="navbar-icons--">
-                      <box-icon name="cart" className="navbar-icons--"></box-icon>
+                    <Link to="/cart">
+                      <box-icon name="cart"></box-icon>
                     </Link>
                   </motion.div>
                 </div>
-                  : <>
-                    <div className="navbar-icons--">
-                      <motion.div
-                        whileTap={{ scale: 0.9 }}>
-                        <Link to="/login">
-                          <box-icon name="user"></box-icon>
-                        </Link>
-                      </motion.div>
-                      <motion.div
-                        whileTap={{ scale: 0.9 }}>
-                        <Link to="/cart">
-                          <box-icon name="cart"></box-icon>
-                        </Link>
-                      </motion.div>
-                    </div>
-                  </>
-              }
+                : <>
+                  <div className="navbar-icons--">
+                    <motion.div
+                      whileTap={{ scale: 0.9 }}>
+                      <Link to="/login">
+                        <box-icon name="user"></box-icon>
+                      </Link>
+                    </motion.div>
+                    <motion.div
+                      whileTap={{ scale: 0.9 }}>
+                      <Link to="/cart">
+                        <box-icon name="cart"></box-icon>
+                      </Link>
+                    </motion.div>
+                  </div>
+                </>}
             </div>
             <div className="navbar-icons">
               {
                 user ? <div className="navbar-icons">
-                  <span>{userName}</span>
+                  <div className="dropdown">
+                    <button
+                      className="btn btn-light men-order-button dropdown-toggle"
+                      type="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      {userName}
+                    </button>
+                    <ul className="dropdown-menu">
+                      <li>
+                        <span className="dropdown-item text-justify" onClick={() => logout()}>Cerrar sesion</span>
+                      </li>
+                    </ul>
+                  </div>
                   <motion.div className="navbar-icons"
                     whileTap={{ scale: 0.9 }}>
                     <Link to="/cart" className="navbar-icons">
-
                       <box-icon name="cart" className="navbar-icons"></box-icon>
                     </Link>
                   </motion.div>
