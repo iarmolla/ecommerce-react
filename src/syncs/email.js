b74ef52c7198601ca336a92ee7495fe72ebdb111
iarmolla <ivan.armolla@gmail.com>
2023-01-24 23:16:50 -0300

    changing api key

--- src/syncs/email.js
+++ src/syncs/email.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 async function validateEmail(email) {
-  return axios.get(`https://emailvalidation.abstractapi.com/v1/?api_key=9f96a31c932642938d44d1067309fa2d&email=${email}`)
+  return axios.get(`https://emailvalidation.abstractapi.com/v1/?YOUR_API_KEY=${email}`)
 }
 
 export default {
